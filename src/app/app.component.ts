3e1e9ad4d8bd911c61e14674ae405f3b27bd1ca6
faraz78691 <mohdfaraz.github@gmail.com>
2024-09-12 17:12:17 +0530

    design_changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tarbiyatul';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.loadExternalScript();
+      }
+    });
+  }
+
+  loadExternalScript() {
+    const scriptElement = document.createElement('script');
+    scriptElement.src = 'assets/js/main.js';
+    scriptElement.onload = () => {
+      // console.log('External script loaded');
+    };
+    document.body.appendChild(scriptElement);
+  }
+
 }
